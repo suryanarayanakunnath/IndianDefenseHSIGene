c894b259e8497b748ad51cf6cbcdd5b3464df60c
suryanarayanakunnath <suryanarayanakunnath@gmail.com>
2025-09-18 10:21:57 +0530

    Create simple_cnn.py

---
+++ models/simple_cnn.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+class SimpleCNN(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, 16, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d(1),
+        )
+        self.fc = nn.Linear(32, num_classes)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
